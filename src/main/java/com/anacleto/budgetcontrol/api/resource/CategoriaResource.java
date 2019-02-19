47dabf1e11ca29a47a770ad341be8d5893035bb7
Tiago Anacleto e Silva <tiago.anacleto@ibm.com>
2019-02-19 11:40:31 -0300

    Added insert and getById categoria

--- src/main/java/com/anacleto/budgetcontrol/api/resource/CategoriaResource.java
+++ src/main/java/com/anacleto/budgetcontrol/api/resource/CategoriaResource.java
@@ -1,11 +1,22 @@
 package com.anacleto.budgetcontrol.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.anacleto.budgetcontrol.api.model.Categoria;
 import com.anacleto.budgetcontrol.api.repository.CategoriaRepository;
@@ -18,7 +29,30 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
-	public List<Categoria> listar() {
+	public List<Categoria> listarCategorias() {
 		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criarCategoria(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+											 .path("/{codigo}")
+											 .buildAndExpand(categoriaSalva.getCodigo())
+											 .toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> getCategoriaByCodigo(@PathVariable Long codigo) {
+		Optional<Categoria> categoriaOp = categoriaRepository.findById(codigo);
+		
+		return categoriaOp.isPresent() ? ResponseEntity.ok().body(categoriaOp.get()) 
+									   : ResponseEntity.notFound().build();
 	}
 }
