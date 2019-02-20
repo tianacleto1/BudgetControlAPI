4d28ac1de408b58b7c3667a66f604879afdc7eb9
Tiago Anacleto e Silva <tiago.anacleto@ibm.com>
2019-02-20 16:03:45 -0300

    Added fields validation with BeanValidation

--- src/main/java/com/anacleto/budgetcontrol/api/resource/CategoriaResource.java
+++ src/main/java/com/anacleto/budgetcontrol/api/resource/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -35,7 +36,7 @@ public class CategoriaResource {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<Categoria> criarCategoria(@RequestBody Categoria categoria, HttpServletResponse response) {
+	public ResponseEntity<Categoria> criarCategoria(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
