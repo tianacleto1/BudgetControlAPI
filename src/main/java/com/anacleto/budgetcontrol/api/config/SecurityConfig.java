719e8e6a58e05d05013735e82422b5a48618307e
Tiago Anacleto <tianacleto1@gmail.com>
2019-04-01 14:05:04 -0300

    Implemented security with oauth2 and Password Flow

--- src/main/java/com/anacleto/budgetcontrol/api/config/SecurityConfig.java
+++ src/main/java/com/anacleto/budgetcontrol/api/config/SecurityConfig.java
@@ -1,31 +1,29 @@
 package com.anacleto.budgetcontrol.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
+@Order(4)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-				.withUser("admin").password("{noop}admin").roles("ROLE");
-	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/categorias").permitAll()
-					.anyRequest().authenticated()
-					.and()
-						.httpBasic()
-					.and()
-						.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-					.and().csrf().disable();
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("admin")
+				.password("{noop}admin")
+				.roles("ROLE");
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 }
