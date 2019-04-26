e1a86d294ade6894fc5552f063b77387449f91d8
Tiago Anacleto <tianacleto1@gmail.com>
2019-04-26 10:24:23 -0300

    Update travis.sql

--- src/test/java/travis.sql
+++ src/test/java/travis.sql
@@ -14,11 +14,6 @@ CREATE TABLE categoria (
 
 # Add Data
 
-CREATE TABLE categoria (
-	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 INSERT INTO categoria (nome) values ('Lazer');
 INSERT INTO categoria (nome) values ('Alimentação');
 INSERT INTO categoria (nome) values ('Supermercado');
