9d2c36f67fb9adfdebda64cd85530079ed49a92b
Tiago Anacleto <tianacleto1@gmail.com>
2019-04-26 09:52:52 -0300

    Added Pipeline with travis

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![Build Status](https://travis-ci.org/tianacleto1/BudgetControl-API) ![Quality Gate Status](https://sonarcloud.io/dashboard?id=com.anacleto%3Abudgetcontrol-api) ![Coverage](https://sonarcloud.io/dashboard?id=com.anacleto%3Abudgetcontrol-api)
+
 # Budget Control-API
 
 This is a simple app for payments and receipts control
---
+++ src/test/java/travis.sql
@@ -0,0 +1,26 @@
+# Create Testuser
+CREATE USER 'dev'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,REFERENCES ON *.* TO 'dev'@'localhost';
+
+# Create DB
+CREATE DATABASE IF NOT EXISTS `budgetcontrol` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `budgetcontrol`;
+
+# Create Table
+CREATE TABLE categoria (
+	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# Add Data
+
+CREATE TABLE categoria (
+	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
